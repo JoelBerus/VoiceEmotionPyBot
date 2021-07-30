1eadc5c9ca5bedc87b33d97fca093928efed6a16
Joel Bermudez Acosta <47182906+JoelBerus16@users.noreply.github.com>
2021-07-30 00:09:05 -0500

    audios y inteligencia

---
+++ AiModel.py
@@ -0,0 +1,61 @@
+import librosa as lb
+import soundfile as sf
+import numpy as np
+import os, glob, pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+emotion_labels = {
+    '01':'happy',
+    '02':'sad',
+}
+
+focused_emotion_labels = ['happy', 'sad']
+
+def audio_features(file_title, mfcc, chroma, mel):
+    #samplerate2 = sf.read("/Users/Asus/Workspace/VoiceEmotionPyBot/tmp/Sujeto1/01-F-01.wav")
+    #print(samplerate2) 
+    with sf.SoundFile(file_title) as audio_recording:
+        audio = audio_recording.read(dtype="float32")
+        sample_rate = audio_recording.samplerate
+        
+        if chroma:
+            stft=np.abs(lb.stft(audio))
+            result=np.array([])
+        if mfcc:
+            mfccs=np.mean(lb.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40).T, axis=0)
+            result=np.hstack((result, mfccs))
+        if chroma:
+            chroma=np.mean(lb.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, chroma))
+        if mel:
+            mel=np.mean(lb.feature.melspectrogram(audio, sr=sample_rate).T,axis=0)
+            result=np.hstack((result, mel))
+        return result
+
+def loading_audio_data():
+    x = []
+    y = []
+    for file in glob.glob("/Users/Asus/Workspace/VoiceEmotionPyBot/tmp/Sujeto1/*.wav"):
+        file_path=os.path.basename(file)
+        emotion = emotion_labels[file_path.split("-")[0]]
+        if emotion not in focused_emotion_labels:
+            continue
+        feature = audio_features(file, mfcc=True, chroma=True, mel=True)
+        
+        x.append(feature)
+        y.append(emotion)
+    final_dataset = train_test_split(np.array(x), y, test_size=0.1, random_state=9)
+    return final_dataset
+
+X_train, X_test, y_train, y_test = loading_audio_data()
+
+model = MLPClassifier(hidden_layer_sizes=(200,), learning_rate='adaptive', max_iter=400)
+
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_true=y_test, y_pred=y_pred)
+print("Accuracy of the Recognizer is: {:.1f}%".format(accuracy*100))
--- VoiceEmotionsPyTelegramBot.py
+++ VoiceEmotionsPyTelegramBot.py
@@ -1,7 +1,28 @@
 import telebot
-bot = telebot.TeleBot("1791987070:AAF6rlODgpy4u0AFrzPor40uBe0NESGq-e0")
-@bot.message_handler(commands=["helps", "start"])
 
+bot = telebot.TeleBot("1791987070:AAF6rlODgpy4u0AFrzPor40uBe0NESGq-e0")
+
+@bot.message_handler(commands=["helps"])
 def enviar(message):
-    bot.reply_to(message, "Hola, ¿como estas?")
+    bot.reply_to(message, "Hola, Soy un bot de el grupo de procesos de soft.")
+
+@bot.message_handler(commands=["start"])
+def enviar(message):
+    bot.reply_to(message, "Hola, bienvenido")
+    
+@bot.message_handler(commands=["saludame"])
+def enviar(message):
+    bot.reply_to(message, "Hola "+ message.from_user.first_name+", ¿como estas?" )
+
+@bot.message_handler(commands=['nude'])
+def send_photo(message):
+    image = open("\tmp\bot.png", 'rb')
+    bot.send_photo(message.chat.id,image )
+    #https://drive.google.com/file/d/1-nJ64_E4_Gmm5iSt7RB86N6D6_HKntZs/view
+    #bot.reply_to(message, "Hola "+ message.from_user.first_name+", ¿como estas?" )
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
 bot.polling()
